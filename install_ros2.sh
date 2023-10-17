7bcc694cb0b196b8b7bcbf891412190d9bf7ef61
coco-server <cocobird231@gmail.com>
2023-10-18 00:13:52 +0800

    Modify install_ros.sh. Update codePack.

--- install_ros2.sh
+++ install_ros2.sh
@@ -51,22 +51,22 @@ else
         sudo apt install ros-eloquent-ros-base -y
     elif [ "$ros_distro" == "foxy" ]
     then
-        sudo apt install software-properties-common
-        sudo add-apt-repository universe
+        sudo apt install software-properties-common -y
+        sudo add-apt-repository universe -y
         sudo apt update && sudo apt install curl -y
         sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
         sudo apt update
-        sudo apt install ros-foxy-ros-base python3-argcomplete
+        sudo apt install ros-foxy-ros-base python3-argcomplete -y
     elif [ "$ros_distro" == "humble" ]
     then
-        sudo apt install software-properties-common
-        sudo add-apt-repository universe
+        sudo apt install software-properties-common -y
+        sudo add-apt-repository universe -y
         sudo apt update && sudo apt install curl -y
         sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
         sudo apt update
-        sudo apt install ros-humble-ros-base
+        sudo apt install ros-humble-ros-base -y
     fi
     echo "ROS2 distro $ros_distro installed."
 
